6192757a3b5cf496d75184b128811989681a8db0
Matheus-Hespagnola <matheusah@ciandt.com>
2025-02-14 08:31:53 -0300

    icon page

--- src/contexts/ThemeContext.tsx
+++ src/contexts/ThemeContext.tsx
@@ -19,7 +19,35 @@ export const ThemeProvider: React.FC<{ children: React.ReactNode }> = ({
     setIsDarkMode(prefersDark);
   }, []);
 
+  useEffect(() => {
+    updateFavicon(isDarkMode);
+  }, [isDarkMode]);
+
   const toggleTheme = () => setIsDarkMode(!isDarkMode);
+
+  const updateFavicon = (isDark: boolean) => {
+    const faviconId = isDark ? 'dark-icon' : 'light-icon';
+    const newFaviconHref = isDark ? '/MH-dark.png' : '/MH-light.png';
+  
+    let link = document.head.querySelector(`link#${faviconId}`) as HTMLLinkElement;
+  
+    if (!link) {
+      link = document.createElement('link');
+      link.id = faviconId;
+      link.rel = 'icon';
+      link.type = 'image/png';
+      document.head.appendChild(link);
+    }
+  
+    link.href = newFaviconHref;
+  
+    // Remover o outro ícone, se existir
+    const otherIconId = isDark ? 'light-icon' : 'dark-icon';
+    const otherIcon = document.head.querySelector(`link#${otherIconId}`);
+    if (otherIcon) {
+      otherIcon.remove();
+    }
+  };
 
   return (
     <ThemeContext.Provider value={{ isDarkMode, toggleTheme }}>
