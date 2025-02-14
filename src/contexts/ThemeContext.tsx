1ce6a7a34af98c4eccb94f283b81fbf528ef201b
Matheus-Hespagnola <matheusah@ciandt.com>
2025-02-14 09:01:36 -0300

    fix icon bug in prod

--- src/contexts/ThemeContext.tsx
+++ src/contexts/ThemeContext.tsx
@@ -17,6 +17,20 @@ export const ThemeProvider: React.FC<{ children: React.ReactNode }> = ({
       "(prefers-color-scheme: dark)"
     ).matches;
     setIsDarkMode(prefersDark);
+
+    const updateInitialFavicon = () => {
+      updateFavicon(prefersDark);
+    };
+
+    // Atualiza o favicon quando o componente monta
+    updateInitialFavicon();
+
+    // Tenta novamente após o carregamento completo da página
+    window.addEventListener("load", updateInitialFavicon);
+
+    return () => {
+      window.removeEventListener("load", updateInitialFavicon);
+    };
   }, []);
 
   useEffect(() => {
@@ -26,27 +40,40 @@ export const ThemeProvider: React.FC<{ children: React.ReactNode }> = ({
   const toggleTheme = () => setIsDarkMode(!isDarkMode);
 
   const updateFavicon = (isDark: boolean) => {
-    const faviconId = isDark ? 'dark-icon' : 'light-icon';
-    const newFaviconHref = isDark ? '/MH-dark.png' : '/MH-light.png';
-  
-    let link = document.head.querySelector(`link#${faviconId}`) as HTMLLinkElement;
-  
-    if (!link) {
-      link = document.createElement('link');
-      link.id = faviconId;
-      link.rel = 'icon';
-      link.type = 'image/png';
-      document.head.appendChild(link);
-    }
-  
-    link.href = newFaviconHref;
-  
-    // Remover o outro ícone, se existir
-    const otherIconId = isDark ? 'light-icon' : 'dark-icon';
-    const otherIcon = document.head.querySelector(`link#${otherIconId}`);
-    if (otherIcon) {
-      otherIcon.remove();
-    }
+    const faviconId = isDark ? "dark-icon" : "light-icon";
+    const newFaviconHref = isDark ? "/MH-dark.png" : "/MH-light.png";
+
+    const updateIcon = () => {
+      let link = document.head.querySelector(
+        `link#${faviconId}`
+      ) as HTMLLinkElement;
+
+      if (!link) {
+        link = document.createElement("link");
+        link.id = faviconId;
+        link.rel = "icon";
+        link.type = "image/png";
+        document.head.appendChild(link);
+      }
+
+      link.href = newFaviconHref;
+
+      // Remover o outro ícone, se existir
+      const otherIconId = isDark ? "light-icon" : "dark-icon";
+      const otherIcon = document.head.querySelector(`link#${otherIconId}`);
+      if (otherIcon) {
+        otherIcon.remove();
+      }
+    };
+
+    // Tenta atualizar o ícone imediatamente
+    updateIcon();
+
+    // Se falhar, tenta novamente após um curto intervalo
+    setTimeout(updateIcon, 100);
+
+    // E novamente após um intervalo maior, caso o DOM ainda não esteja totalmente carregado
+    setTimeout(updateIcon, 1000);
   };
 
   return (
